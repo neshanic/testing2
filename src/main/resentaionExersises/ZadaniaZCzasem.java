02a3cb340986b1c4077e3d578943caa99065f2d6
Rafał Kostecki <kiwi@gazeta.pl>
2020-06-20 13:31:17 +0200

    zadanie z czasem

---
+++ src/main/resentaionExersises/ZadaniaZCzasem.java
@@ -0,0 +1,34 @@
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class ZadaniaZCzasem {
+
+    public static void dateNow() {
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println("LocalDateTime: " + ldt);
+        Instant now = Instant.now();
+        System.out.println("Instant: " + now);
+    }
+
+    public static void tokyoDateTime() {
+        // Strefy dokumentacja https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/
+        Clock clock = Clock.system(ZoneId.of("Asia/Tokyo"));
+        LocalDateTime ldt = LocalDateTime.now(clock);
+        System.out.println("Japan time:" + ldt);
+    }
+
+    public static void dateFormat() {
+        LocalDateTime ldt = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss");
+        System.out.println("Date in format yyyy/mm/dd yyyy/mm/dd hh:mm:ss: " + formatter.format(ldt));
+    }
+
+    public static void main(String[] args) {
+        dateNow();
+        tokyoDateTime();
+        dateFormat();
+    }
+}
